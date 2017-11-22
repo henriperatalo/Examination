54d33fe0142a033676b73db98c97aeb988804d39
henriperatalo <32446796+henriperatalo@users.noreply.github.com>
2017-11-22 12:52:14 +0200

    Add files via upload

---
+++ linkedlist.c
@@ -0,0 +1,125 @@
+#include "linkedlist.h"
+
+int add_to_list(linked_list *ll, char *s)
+{
+	linked_list *newll=ll;
+	int i=0;
+	while (newll->next != NULL)
+		{
+			newll = newll->next;
+			i++;
+		}
+	i++;
+	newll->next = (linked_list* )malloc(sizeof(struct linked_list));
+    newll->next->data = s;
+    newll->next->next = NULL;
+	newll->next->index = i;
+
+
+//	struct linked_list *newll
+//	newll = (linked_list* )malloc(sizeof(struct linked_list));
+//	newll->data = s;
+//	ll->next = newll;
+//	newll->next = NULL;	
+//	newll->index=ll->index;
+//	newll->index++;
+	
+	
+	return newll->next->index;
+};
+
+int display_item(linked_list *ll)
+{
+	if(ll->data != NULL)
+		{
+			printf("%s\n",ll->data);
+			return 0;
+		}
+	else 
+		return -1;
+
+};
+
+int display_list(linked_list *ll)
+{
+	linked_list *newll=ll;
+	int i=0;
+	while (newll->next != NULL)
+		{
+			newll = newll->next;
+			i++;
+			display_item(newll);
+		}
+	return i;
+};
+
+linked_list * search_from_list(linked_list *ll, char *s)
+{
+	linked_list *newll=ll;
+	while (newll->next != NULL)
+		{
+			newll = newll->next;
+			if(newll->data==s)
+				return newll;			
+		}
+	return NULL;
+};
+
+int delete_from_list(linked_list *ll, int index)
+{
+	int i;
+	linked_list * newll = ll;
+	linked_list * temp = NULL;
+
+	if (index<=0)
+		return -1;
+	else
+		{
+			for (i = 0; i < index-1; i++)
+				{
+					if (newll->next == NULL)
+						{
+							if(newll->index==index-1)
+							{
+								free(newll);
+								ll->next=NULL;
+								return 0;
+							}
+							return -1;
+						}
+					newll = newll->next;
+				}
+
+    temp = newll->next;
+    newll->next = temp->next;
+    free(temp);
+	i=0;
+	newll = ll;
+	
+	while (newll->next != NULL)
+		{
+			newll = newll->next;
+			i++;
+		}
+			return i;
+		}
+};
+
+int empty_list(linked_list *ll)
+{
+	if (ll->next == NULL) 
+	{
+        free(ll);
+        return 0;
+    }
+ 
+	linked_list * current = ll;	
+    while (current->next->next != NULL) 
+		{
+			current = current->next;
+		}
+
+    free(current->next);
+    current->next = NULL;
+	return 0;
+};
---
+++ makefile
@@ -0,0 +1,20 @@
+GTEST_DIR = /u/h/e1401187/Kurssit/EmbeddedSystems/googletest-master/googletest
+GTEST_INCLUDE=-I $(GTEST_DIR)/include
+
+my_test: test_case.o linkedlist.o linkedlist.o Gtest_main.c libgtest.a
+	g++ -isystem $(GTEST_DIR)/include -pthread test_case.o linkedlist.o Gtest_main.c libgtest.a -o my_test --coverage
+
+test_case.o: test_case.c
+	g++ -c $(GTEST_INCLUDE) test_case.c --coverage
+	
+linkedlist.o: linkedlist.c linkedlist.h
+	g++ -c linkedlist.c  --coverage
+	
+libgtest.a: gtest-all.o
+	ar -rv libgtest.a gtest-all.o
+	
+gtest-all.o:
+	g++ -isystem $(GTEST_DIR)/include -I $(GTEST_DIR) -pthread -c $(GTEST_DIR)/src/gtest-all.cc
+
+clean:
+	rm my_test *.o libgtest.a
